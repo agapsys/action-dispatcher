052069585943c537f1220c45cba437deb7ea1e41
Leandro Oliveira <leandro@agapsys.com>
2015-09-15 10:58:06 -0300

    Added WebServlet annotation

--- src/main/java/com/agapsys/web/ActionDispatcherServlet.java
+++ src/main/java/com/agapsys/web/ActionDispatcherServlet.java
@@ -18,10 +18,12 @@ package com.agapsys.web;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/*")
 public class ActionDispatcherServlet extends HttpServlet {
 	// CLASS SCOPE =============================================================
 	private static final ActionDispatcher dispatcher = new ActionDispatcher();
